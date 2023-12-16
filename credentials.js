0c64c6483edfcda28895a68d9a13e27997fa65f6
VishalGoel11 <vishugoel2812@gmail.com>
2023-12-16 18:59:27 +0530

    initial commit

--- credentials.js
+++ credentials.js
@@ -1,9 +1,6 @@
 
-/*
- How this obtained ? ->see the readme.md file 
-*/
-const CLIENT_ID = "Your Client Id";
-const CLEINT_SECRET = "Your Client Secret";
+const CLIENT_ID = "1061599156063-fh0ig667gunejbc6abi9evdihvqqonn6.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN ="Your Refresh Token";
+const REFRESH_TOKEN ="1//04xYnOmzcPIssCgYIARAAGAQSNwF-L9Ir4-JhVl4Rx7yWJRo3cEHZx68ZFip0yUhtHv7SoUYNRnwRI7BzLYfsX64_drrihtYa0L4";
 module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
